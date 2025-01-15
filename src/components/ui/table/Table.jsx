6031a0ad5b0d89073ee0d77cd796f13f304892a1
bintang ary <wardhanabintang07@gmail.com>
2025-01-15 20:46:22 +0700

    Render semua data di tabel 'coin by market cap'

--- src/components/ui/table/Table.jsx
+++ src/components/ui/table/Table.jsx
@@ -1,5 +1,6 @@
 import { MdNumbers } from "react-icons/md";
 import { FaCaretUp } from "react-icons/fa";
+import { FaCaretDown } from "react-icons/fa";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import LineChart from "./LineChart";
@@ -7,6 +8,7 @@ import LineChart from "./LineChart";
 const Table = () => {
     const [coinsBMC, setCoinsBMC] = useState([]);
 
+    // get coins by market cap
     useEffect(() => {
         const getCoinsBMC = async () => {
             try {
@@ -30,6 +32,14 @@ const Table = () => {
         getCoinsBMC();
     }, []);
 
+    const formatToRupiah = (value) => {
+        return new Intl.NumberFormat("id-ID", {
+            style: "currency",
+            currency: "IDR",
+            minimumFractionDigits: 0, // Jika tidak ingin desimal
+        }).format(value);
+    };
+
     return (
         <>
             <div className="overflow-x-auto">
@@ -41,42 +51,85 @@ const Table = () => {
                             </th>
                             <th className="w-40 md:w-64 text-left pl-5 sticky left-12 bg-[#0f0e14] z-10">Coin</th>
                             <th className="w-32 text-right">Price</th>
-                            <th className="w-12 text-center pr-0">1h</th>
-                            <th className="w-12 text-center pr-0">24h</th>
-                            <th className="w-12 text-center pr-0">7d</th>
+                            <th className="w-16 text-center pr-0">1h</th>
+                            <th className="w-16 text-center pr-0">24h</th>
+                            <th className="w-16 text-center pr-0">7d</th>
                             <th className="w-36 text-right truncate">24h Volume</th>
                             <th className="w-36 text-right truncate">Market Cap</th>
-                            <th className="w-36 text-right truncate">Last 7 Days</th>
+                            <th className="w-32">Last 7 Days</th>
                         </tr>
                     </thead>
                     <tbody>
                         {coinsBMC.map((coin) => {
+                            const lastPrice = coin.sparkline_in_7d.price.at(-1);
+                            const price1HourAgo = coin.sparkline_in_7d.price.at(-7);
+                            const price7DaysAgo = coin.sparkline_in_7d.price[0];
+
+                            const priceChange1h = ((lastPrice - price1HourAgo) / price1HourAgo) * 100 || 0;
+                            const priceChange24h = coin.price_change_percentage_24h || 0;
+                            const priceChange7d = ((lastPrice - price7DaysAgo) / price7DaysAgo) * 100 || 0;
                             const prices = coin.sparkline_in_7d.price;
                             const totalData = prices.length;
                             const interval = Math.floor(totalData / 10);
                             const sampledData = prices.filter((_, index) => index % interval === 0).slice(0, 10);
 
+                            // Function to determine color and symbol
+                            const getChangeIndicator = (value) => {
+                                if (value < 0) {
+                                    return { color: "text-red-400", icon: <FaCaretDown /> };
+                                }
+                                return { color: "text-green-400", icon: <FaCaretUp /> };
+                            };
+                            const change1h = getChangeIndicator(priceChange1h);
+                            const change24h = getChangeIndicator(priceChange24h);
+                            const change7d = getChangeIndicator(priceChange7d);
+
                             return (
                                 <tr key={coin.id}>
+                                    {/* number */}
                                     <td className="text-center sticky left-0 bg-[#0f0e14] z-10">#</td>
 
+                                    {/* name */}
                                     <td className="flex item-center h-auto text-left pl-5 sticky left-12 bg-[#0f0e14] z-10">
                                         <img src={coin.image} className="w-7 h-7" alt={coin.name} />
                                         <span className="pl-2 my-auto">{coin.name}</span>
-                                        <span className=" px-1 my-auto text-sm"> &#x2022; {coin.symbol}</span>
+                                        <span className=" px-1 my-auto text-sm text-gray-700"> &#x2022; {coin.symbol}</span>
                                     </td>
 
-                                    <td className="text-right">{coin.current_price}</td>
-                                    <td className="text-center pr-0 flex items-center text-green-400">
-                                        <span>
-                                            <FaCaretUp />
-                                        </span>
-                                        1.5%
+                                    {/* price */}
+                                    <td className="text-right">{formatToRupiah(coin.current_price)}</td>
+
+                                    {/* 1h changes */}
+                                    <td className={`text-center pr-0 ${change1h.color}`}>
+                                        <div className="flex items-center">
+                                            <span>{change1h.icon}</span>
+                                            <div>{priceChange1h.toFixed(1)}%</div>
+                                        </div>
                                     </td>
-                                    <td className="text-center pr-0">10</td>
-                                    <td className="text-center pr-0">10</td>
-                                    <td className="text-right truncate">100,000,000,000</td>
-                                    <td className="text-right truncate">100,000,000,000</td>
+
+                                    {/* 24h changes */}
+                                    <td className={`text-center pr-0 ${change24h.color}`}>
+                                        <div className="flex items-center">
+                                            <span>{change24h.icon}</span>
+                                            <div>{priceChange24h.toFixed(1)}%</div>
+                                        </div>
+                                    </td>
+
+                                    {/* 7d changes */}
+                                    <td className={`text-center pr-0 ${change7d.color}`}>
+                                        <div className="flex items-center">
+                                            <span>{change7d.icon}</span>
+                                            <div>{priceChange7d.toFixed(1)}%</div>
+                                        </div>
+                                    </td>
+
+                                    {/* 24h volume */}
+                                    <td className="text-right truncate">{formatToRupiah(coin.total_volume)}</td>
+
+                                    {/* market cap */}
+                                    <td className="text-right truncate">{formatToRupiah(coin.market_cap)}</td>
+
+                                    {/* 7days chart */}
                                     <td className="text-right truncate p-0">
                                         <LineChart coinName={coin.name} coinPrice={sampledData} />
                                     </td>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import ByMarketCap from "../../components/content/by-market-cap/ByMarketCap";
 
 const Home = () => {
     return (
-        <div className="max-w-[1200px] container mx-auto">
+        <div className="max-w-[1400px] container mx-auto md:text-base">
             <Navbar />
             <ByMarketCap />
         </div>
